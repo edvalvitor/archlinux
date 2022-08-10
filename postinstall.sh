58141e1bd1513535719f3e21fade82f96e7ad48d
edvalvitor <edval.vitor@gmail.com>
2022-08-10 14:41:06 -0300

    modificando postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sudo pacman -S network-manager-applet xfce4-terminal rofi nitrogen volumeicon compton polybarxfce4-screenshooter terminus-font gnome-themes-extra
-
+sudo pacman -S network-manager-applet xfce4-terminal rofi nitrogen volumeicon compton polybarxfce4-screenshooter terminus-font gnome-themes-extra papirus-icon-theme
