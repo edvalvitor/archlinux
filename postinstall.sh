6e3223b47672201a50143a0651253a996a9df297
edvalvitor <edval.vitor@gmail.com>
2022-08-11 19:49:54 -0300

    battery module polybar

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo pacman -S git i3status brightnessctl network-manager-applet xfce4-terminal rofi nitrogen volumeicon compton xfce4-screenshooter terminus-font gnome-themes-extra papirus-icon-theme adapta-gtk-theme lxappearance thunar arandr ttf-ubuntu-font-family pulseaudio xss-lock i3lock
+sudo pacman -S git i3lock polybar brightnessctl network-manager-applet rofi nitrogen volumeicon xfce4-screenshooter terminus-font gnome-themes-extra papirus-icon-theme adapta-gtk-theme lxappearance thunar arandr ttf-ubuntu-font-family pulseaudio xss-lock i3lock
