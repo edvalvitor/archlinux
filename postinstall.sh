f2f953ba4bbc9b7dece77b9440214d606c8a2889
edvalvitor <edval.vitor@gmail.com>
2022-08-11 08:34:45 -0300

    alterações

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo pacman -S git brightnessctl network-manager-applet xfce4-terminal rofi nitrogen volumeicon compton xfce4-screenshooter terminus-font gnome-themes-extra papirus-icon-theme adapta-gtk-theme lxappearance thunar arandr ttf-ubuntu-font-family pulseaudio
+sudo pacman -S git i3status brightnessctl network-manager-applet xfce4-terminal rofi nitrogen volumeicon compton xfce4-screenshooter terminus-font gnome-themes-extra papirus-icon-theme adapta-gtk-theme lxappearance thunar arandr ttf-ubuntu-font-family pulseaudio
