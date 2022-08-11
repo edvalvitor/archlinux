9af86a63a922086806d47bd07701d30570d0a71e
edvalvitor <edval.vitor@gmail.com>
2022-08-11 20:43:21 -0300

    battery polybar

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo pacman -S git i3lock polybar brightnessctl network-manager-applet rofi nitrogen volumeicon xfce4-screenshooter terminus-font gnome-themes-extra papirus-icon-theme adapta-gtk-theme lxappearance thunar arandr ttf-ubuntu-font-family pulseaudio xss-lock i3lock
+sudo pacman -S git polybar brightnessctl network-manager-applet rofi nitrogen volumeicon xfce4-screenshooter terminus-font gnome-themes-extra papirus-icon-theme adapta-gtk-theme lxappearance thunar arandr ttf-ubuntu-font-family pulseaudio xss-lock i3lock imagemagick 
