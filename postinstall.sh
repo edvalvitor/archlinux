1f36b86d4f30070f9a75082f5226a8c8767b62fb
edvalvitor <edval.vitor@gmail.com>
2022-08-10 22:25:29 -0300

    post

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo pacman -S network-manager-applet xfce4-terminal rofi nitrogen volumeicon compton xfce4-screenshooter terminus-font gnome-themes-extra papirus-icon-theme adapta-gtk-theme lxappearance thunar
+sudo pacman -S git brightnessctl network-manager-applet xfce4-terminal rofi nitrogen volumeicon compton xfce4-screenshooter terminus-font gnome-themes-extra papirus-icon-theme adapta-gtk-theme lxappearance thunar arandr ttf-ubuntu-font-family pulseaudio
