dbd7cff40f4606cc82b6c4fea1001f9152c6267a
edvalvitor <edval.vitor@gmail.com>
2022-08-10 22:16:42 -0300

    Alterações

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo pacman -S network-manager-applet xfce4-terminal rofi nitrogen volumeicon compton polybarxfce4-screenshooter terminus-font gnome-themes-extra papirus-icon-theme adapta-gtk-theme
+sudo pacman -S network-manager-applet xfce4-terminal rofi nitrogen volumeicon compton xfce4-screenshooter terminus-font gnome-themes-extra papirus-icon-theme adapta-gtk-theme lxappearance thunar
