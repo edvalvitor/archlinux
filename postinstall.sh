b83cfd5b72ecfd72741c8b8e4b6c8020ba7a5d78
edvalvitor <edval.vitor@gmail.com>
2022-08-30 16:53:33 -0300

    flameshot

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo pacman -S git polybar brightnessctl network-manager-applet rofi nitrogen volumeicon xfce4-screenshooter terminus-font gnome-themes-extra papirus-icon-theme adapta-gtk-theme lxappearance thunar arandr pulseaudio xss-lock i3lock imagemagick xdg-desktop-portal
+sudo pacman -S git polybar brightnessctl network-manager-applet rofi nitrogen volumeicon xfce4-screenshooter terminus-font gnome-themes-extra papirus-icon-theme adapta-gtk-theme lxappearance thunar arandr pulseaudio xss-lock i3lock imagemagick xdg-desktop-portal flameshot
