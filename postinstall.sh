9f086efe53d4fed43274ea1dd3dfbae9207b1ab3
edvalvitor <edval.vitor@gmail.com>
2022-08-31 20:37:58 -0300

    postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo pacman -S git polybar brightnessctl network-manager-applet rofi nitrogen volumeicon terminus-font gnome-themes-extra papirus-icon-theme adapta-gtk-theme lxappearance thunar arandr xss-lock i3lock imagemagick xdg-desktop-portal flameshot copyq
+sudo pacman -S git polybar brightnessctl network-manager-applet rofi nitrogen volumeicon terminus-font gnome-themes-extra papirus-icon-theme adapta-gtk-theme lxappearance thunar arandr xss-lock i3lock imagemagick xdg-desktop-portal flameshot copyq xreader ristretto vlc
