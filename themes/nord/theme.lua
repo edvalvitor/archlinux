e024e8f5e7bdb696b4545deed3e5df926b27415f
edvalvitor <edval.vitor@gmail.com>
2022-09-04 17:21:51 -0300

    nord border focused window

--- themes/nord/theme.lua
+++ themes/nord/theme.lua
@@ -25,9 +25,9 @@ theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
 theme.useless_gap   = dpi(0)
-theme.border_width  = dpi(2)
+theme.border_width  = dpi(1)
 theme.border_normal = "#2e3440"
-theme.border_focus  = "#BF616A"
+theme.border_focus  = "#ffffff"
 theme.border_marked = "#91231c"
 
 -- There are other variable sets
