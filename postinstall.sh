09c6b3dc332facfb497921a3e75f9334a2e39024
edvalvitor <edval.vitor@gmail.com>
2022-09-06 12:05:10 -0300

    post install

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 sudo pacman -S git polybar brightnessctl network-manager-applet rofi nitrogen volumeicon terminus-font gnome-themes-extra papirus-icon-theme adapta-gtk-theme lxappearance thunar arandr xss-lock i3lock imagemagick xdg-desktop-portal flameshot copyq xreader ristretto vlc
+
+git #github
+brightnessctl #brightness
+network-manager-applet #network settings
+nitrogen #wallpaper
+volumeicon #volume control
+terminus-font #font
+gnome-theme-extras #themes
+papirus-icon-theme #icons
+lxappearance #gtk themes managment
+thunar # file manager
+arandr #monitores
+xdg-desktop-portal #file picker
+flameshot #screenshot
+copyq #clipboard
+xreader #pdf
+ristretto #imagens
+vlc #video
+blueman #bluetooth
