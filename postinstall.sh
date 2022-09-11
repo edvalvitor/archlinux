5219fa476f578550b8558cbacc9936fa2281afd2
edvalvitor <edval.vitor@gmail.com>
2022-09-11 13:30:17 -0300

    i3

--- postinstall.sh
+++ postinstall.sh
@@ -2,6 +2,7 @@
 sudo pacman -S git polybar brightnessctl network-manager-applet rofi nitrogen volumeicon terminus-font gnome-themes-extra papirus-icon-theme adapta-gtk-theme lxappearance thunar arandr xss-lock i3lock imagemagick xdg-desktop-portal flameshot copyq xreader ristretto vlc
 
 git #github
+duf #disk information
 brightnessctl #brightness
 network-manager-applet #network settings
 nitrogen #wallpaper
@@ -10,7 +11,7 @@ terminus-font #font
 gnome-theme-extras #themes
 papirus-icon-theme #icons
 lxappearance #gtk themes managment
-thunar # file manager
+nemo # file manager
 arandr #monitores
 xdg-desktop-portal #file picker
 flameshot #screenshot
