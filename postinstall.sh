447ff9c2e334ec1a8156f87c9c7279f2e98b14ea
edvalvitor <edval.vitor@gmail.com>
2022-09-18 18:19:49 -0300

    gruvbox

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo pacman -S git polybar brightnessctl network-manager-applet rofi nitrogen volumeicon terminus-font gnome-themes-extra papirus-icon-theme adapta-gtk-theme lxappearance thunar arandr xss-lock i3lock imagemagick xdg-desktop-portal flameshot copyq xreader ristretto vlc
 
 git #github
 duf #disk information
